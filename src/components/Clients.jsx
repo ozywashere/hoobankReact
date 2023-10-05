efc5abad04eae49b871eee5714b1bf773d460afa
ozywashere <durduarslantas@ogr.iu.edu.tr>
2023-10-05 22:43:19 +0300

    some bugs fixed

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -2,7 +2,7 @@ import { clients } from "../constants";
 import styles from "../style";
 export const Clients = () => {
     return (
-        <section className={`${styles.flexCenter} my-4`}>
+        <section className={`${styles.flexCenter} my-4`} id="clients">
             <div className={`${styles.flexCenter} flex-wrap w-full`}>
                 {clients.map((client) => (
                     <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
